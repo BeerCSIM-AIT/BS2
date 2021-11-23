201db04e3d7057b62372769190da3a8e2c4ba588
BeerCSIM_AIT <st121032@ait.ac.th>
2021-11-23 16:55:22 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number = 5;
+  n2: number = 2;
+  picName:string =  "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_1280.jpg";
+
+  hi(name:string){
+    alert("Hi " + name);
+  }
 }
